08eee61fda07c2eac434aaa12894944a28daf8aa
thummar96 <kinjalthummar96@gmail.com>
2022-11-16 14:30:30 +0530

    first coomite

---
+++ JavaScript/C.js
@@ -0,0 +1,128 @@
+// var a=10,i,k=1;
+// if(a>10)
+// {
+//     console.log("a is largest");
+// }
+// else
+// {
+//     console.log("a is samlest");
+// }
+
+// for(i=1;i<=5;i++)
+// {
+//     console.log("%d",k++);
+// }
+
+
+
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log("Hello kinjal");
+//     if (i < 10) {
+//         console.log("In for loop");
+//     }
+//     else {
+//         console.log("Exit for loop");
+//     }
+// }
+
+
+// var n = 5;
+// var string = "";
+
+// for (var i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//         string += "*";
+//     }
+
+//     string += "\n";
+// }
+// console.log(string);
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+
+// var n = 5;
+// var string = "";
+
+// for (var i = 0; i < n; i++) {
+//     for (let j = 0; j < i; j++) {
+//         string += "*";
+//     }
+
+//     string += "\n";
+// }
+// console.log(string);
+
+
+// *
+// **
+// ***
+// ****
+
+
+// var n = 5;
+// var string = "";
+
+// for (var i = 0; i < n; i++) {
+//     for (var j = 0; j < n; j++) {
+//         if (i === 0 || i === n - 1) {
+//             string += "*";
+//         }
+//         else {
+//             if (j === 0 || j === n - 1) {
+//                 string += "*";
+//             }
+//             else {
+//                 string += " ";
+//             }
+//         }
+//     }
+
+//     string += "\n";
+// }
+
+// console.log(string);
+
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+
+// function sum(a, b) {
+//     return a + b;
+// }
+// function sub(a, b) {
+//     return a - b;
+// }
+// function mul(a, b) {
+//     return a * b;
+// }
+// function div(a, b) {
+//     return a / b;
+// }
+// function mod(a, b) {
+//     return a % b;
+// }
+// console.log("a + b =", sum(20, 10));
+// console.log("a - b =", sub(20, 10));
+// console.log("a * b =", mul(20, 10));
+// console.log("a / b =", div(20, 10));
+// console.log("a % b =", mod(20, 10));
+
+
+// a + b = 30
+// a - b = 10
+// a * b = 200
+// a / b = 2
+// a % b = 0
+
+
+
