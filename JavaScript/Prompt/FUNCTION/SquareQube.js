5e69504f4e40baa65c43215e6886cbf0fc82e8fa
thummar96 <kinjalthummar96@gmail.com>
2022-11-25 15:28:47 +0530

    first

--- JavaScript/Prompt/FUNCTION/SquareQube.js
+++ JavaScript/Prompt/FUNCTION/SquareQube.js
@@ -3,54 +3,20 @@
 //  Named function
 
 
-function SquareQube(array)
-{
-    let Squareresult=[];
-    let Quberesult=[];
-    let result=[];
+// function SquareQube(array)
+// {
+//     let Squareresult=[];
+//     let Quberesult=[];
+//     let result=[];
         
-        for(let i=0;i<array.length;i++)
-        {
-               Squareresult.push(array[i]*array[i]);
-               Quberesult.push(array[i]*array[i]*array[i]);
-            }
-            return result=[Squareresult,Quberesult];
-    }
-    console.log(" Number of Square And Qube : ",SquareQube([1,2,3,4,5]));
-
-
-
-// function SquareQube(array) {
-//     let squareresult = [];
-//     let quberesult = [];
-//     let finalarray = [];
-//     for (let i = 0; i < array.lenght; i++) {
-//         squareresult.push(array[i] * array[i]);
-//         quberesult.push(array[i] * array[i] * array[i]);
+//         for(let i=0;i<array.length;i++)
+//         {
+//                Squareresult.push(array[i]*array[i]);
+//                Quberesult.push(array[i]*array[i]*array[i]);
+//             }
+//             return result=[Squareresult,Quberesult];
 //     }
-//     // Resultarray = [Squarearray, Qubearray];
-//     return finalarray=[squareresult, quberesult];
-
-// }
-
-// // let Arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// console.log(SquareQube([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));     // vrushabh
-
-
-
-// function SquareQube(array){
-//     let squareresult=[];
-//     let quberesult=[];
-//     let finalarray=[];
-//     for(let i=0;i<array.length;i++)
-//     {
-//         squareresult.push(array[i]*array[i]);
-//         quberesult.push(array[i]*array[i]*array[i]);
-//     }
-//     return finalarray=[squareresult,quberesult];
-
-// }
-// console.log("Number of Square And Cube : ",SquareQube([1,2,3,4,5]));
+//     console.log(" Number of Square And Qube : ",SquareQube([1,2,3,4,5]));
 
 
 
